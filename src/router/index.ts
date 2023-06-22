5439268d8c17a9785871aaf3406511e4d956e45e
muhammad yulasfi pahrizal <muhammadpahrizal790@gmail.com>
2023-06-22 20:36:16 +0800

    admin live

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,12 +9,12 @@ const router = createRouter({
       component: () => import('../views/DashboardView.vue')
   },
     {
-      path: '/admin',
-      name: 'admin',
+      path: '/live',
+      name: 'live',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AdminView.vue')
+      component: () => import('../views/AdminLiveView.vue')
     }
   ]
 })
