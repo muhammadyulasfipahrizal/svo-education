c2a413c96c2b4245e80ab4592eda9c30bc38fcb2
muhammad yulasfi pahrizal <muhammadpahrizal790@gmail.com>
2023-08-02 14:08:38 +0000

    Update admin dashboard new

--- src/views/Admin/AdminDashboard/AdminDashboard.vue
+++ src/views/Admin/AdminDashboard/AdminDashboard.vue
@@ -33,16 +33,16 @@ export default {
 
                 <div class="flex flex-column">
                     <div class="flex flex-row justify-content-between">
-                        <p class="font-medium font-semibold text-2xl">Ongoing Courses</p>
-                        <p class="font-medium font-semibold text-xl">See all <i class="font-medium pi pi-arrow-right"></i></p>
+                        <p class="font-bold text-2xl">Ongoing Courses</p>
+                        <p class="font-bold text-xl">See all <i class="font-medium pi pi-arrow-right"></i></p>
                     </div>
-                    <DataTable :value="courseList"  paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
-                    dataKey="id" class="shadow-2">
+                    <DataTable :value="courseList" :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
+                    dataKey="id" class="shadow-2" tableStyle="min-width: 40rem">
                         <Column field="course" header="Course">
                             <template #body="value">
                                 <div class="flex gap-2 align-items-center">
                                     <div class="flex gap-1 flex-column align-items-center">
-                                        <h4 class="font-medium">{{ value.data.name }}</h4>
+                                        <p class="text-lg font-medium">{{ value.data.name }}</p>
                                     </div>
                                 </div>
                             </template>
@@ -51,7 +51,7 @@ export default {
                             <template #body="value">
                                 <div class="flex gap-2 align-items-center">
                                     <div class="flex gap-1 flex-column align-items-center">
-                                        <h4 class="font-medium">{{ value.data.instructor }}</h4>
+                                        <p class="text-lg font-medium">{{ value.data.instructor }}</p>
                                     </div>
                                 </div>
                             </template>
@@ -60,8 +60,8 @@ export default {
                             <template #body="value">
                                 <div class="flex gap-2 align-items-center">
                                     <div class="flex gap-1 flex-column align-items-center">
-                                        <h4 class="font-medium">{{ value.data.attandance }}/100</h4>
-                                        <p class="font-medium">students</p>
+                                        <p class="text-lg font-medium">{{ value.data.attandance }}/100</p>
+                                        <p class="text-sm font-medium text-gray">students</p>
                                     </div>
                                 </div>
                             </template>
@@ -72,10 +72,10 @@ export default {
 
                 <div class="flex flex-column">
                     <div class="flex flex-row justify-content-between">
-                        <p class="font-medium font-semibold text-2xl">Top Courses</p>
-                        <p class="font-medium font-semibold text-xl">See all <i class="font-medium pi pi-arrow-right"></i></p>
+                        <p class="font-bold text-2xl">Top Courses</p>
+                        <p class="font-bold text-xl">See all <i class="font-medium pi pi-arrow-right"></i></p>
                     </div>
-                    <DataTable :value="courseList"  paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
+                    <DataTable :value="courseList" :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
                     dataKey="id" class="shadow-2">
                         <Column field="position" header="Position">
                             <template #body="value">
@@ -94,10 +94,10 @@ export default {
                                 <div class="flex gap-2 align-items-center">
                                     <div class="flex gap-1 flex-column align-items-center">
                                         <div class="flex flex-row align-items-center gap-4">
-                                            <img :src="value.data.image" alt="" style="width: 80px; height: 60px;">
+                                            <img :src="value.data.image" alt="" style="width: 113px; height: 70px;">
                                             <div class="flex flex-column">
-                                                <p class="font-medium">{{ value.data.name }}</p>
-                                                <p class="font-medium">by {{ value.data.instructor }}</p>
+                                                <p class="text-base font-medium">{{ value.data.name }}</p>
+                                                <p class="text-sm font-medium text-gray">by {{ value.data.instructor }}</p>
                                             </div>
                                         </div>
                                     </div>
@@ -107,21 +107,22 @@ export default {
                         <Column field="ratings" header="Ratings">
                             <template #body="value">
                                 <div class="flex gap-2 align-items-center">
-                                    <div class="flex gap-1 flex-column align-items-center">
+                                    <div class="flex flex-column align-items-center">
                                         <div class="flex flex-row align-items-center gap-2">
                                             <i class="pi pi-star-fill"></i>
                                             <p class="font-medium">{{ value.data.name }}</p>
                                         </div>
                                         
-                                        <p class="font-medium">{{ value.data.rating.rating }}/5 ({{ value.data.rating.total_rating }} ratings)</p>
+                                        <p class="text-sm font-medium text-gray">{{ value.data.rating.rating }}/5 ({{ value.data.rating.total_rating }} ratings)</p>
                                     </div>
                                 </div>
                             </template>
                         </Column>
                         <Column field="enrolled" header="Enrolled">
                             <template #body="value">
-                                <div class="flex gap-2 align-items-center">
+                                <div class="flex flex-column align-items-center">
                                     <p class="font-medium">{{ value.data.enrolled }}</p>
+                                    <p class="text-sm font-medium text-gray">students</p>
                                 </div>
                             </template>
                         </Column>
@@ -130,10 +131,10 @@ export default {
 
                 <div class="flex flex-column">
                     <div class="flex flex-row justify-content-between">
-                        <p class="font-medium font-semibold text-2xl">Top Instructor</p>
-                        <p class="font-medium font-semibold text-xl">See all <i class="font-medium pi pi-arrow-right"></i></p>
+                        <p class="font-bold text-2xl">Top Instructor</p>
+                        <p class="font-bold text-xl">See all <i class="font-medium pi pi-arrow-right"></i></p>
                     </div>
-                    <DataTable :value="instructorList"  paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
+                    <DataTable :value="instructorList"  :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
                     dataKey="id" class="shadow-2">
                         <Column field="position" header="Position">
                             <template #body="value">
@@ -154,7 +155,7 @@ export default {
                                         <div class="flex flex-row align-items-center gap-4">
                                             <img :src="value.data.image" alt="" style="width: 40px; height: 40px;">
                                             <div class="flex flex-column">
-                                                <p class="font-medium">{{ value.data.name }}</p>
+                                                <p class="text-lg font-medium">{{ value.data.name }}</p>
                                             </div>
                                         </div>
                                     </div>
@@ -166,9 +167,9 @@ export default {
                                 <div class="flex gap-2 align-items-center">
                                     <div class="flex gap-1 flex-column align-items-center">
                                         <div class="flex flex-row align-items-center gap-4">
-                                            <div class="flex flex-column">
-                                                <p class="font-medium">{{ value.data.rating }} %</p>
-                                                <p class="font-medium">Overall Grade</p>
+                                            <div class="flex flex-column align-items-center">
+                                                <p class="text-lg font-medium">{{ value.data.rating }} %</p>
+                                                <p class="text-sm font-meidum text-gray font-medium">Overall Grade</p>
                                             </div>
                                         </div>
                                     </div>
@@ -179,12 +180,10 @@ export default {
                             <template #body="value">
                                 <div class="flex gap-2 align-items-center">
                                     <div class="flex gap-1 flex-column align-items-center">
-                                        <div class="flex flex-row align-items-center gap-4">
-                                            <div class="flex flex-column">
-                                                <p class="font-medium">{{ value.data.enrolled }}</p>
-                                                <p class="font-medium">Courses</p>
+                                            <div class="flex flex-column align-items-center">
+                                                <p class="text-lg font-medium">{{ value.data.enrolled }}</p>
+                                                <p class="text-sm font-medium text-gray font-medium">Courses</p>
                                             </div>
-                                        </div>
                                     </div>
                                 </div>
                             </template>
@@ -194,10 +193,10 @@ export default {
 
                 <div class="flex flex-column">
                     <div class="flex flex-row justify-content-between">
-                        <p class="font-medium font-semibold text-2xl">Top Students</p>
-                        <p class="font-medium font-semibold text-xl">See all <i class="font-medium pi pi-arrow-right"></i></p>
+                        <p class="font-bold text-2xl">Top Students</p>
+                        <p class="font-bold text-xl">See all <i class="font-medium pi pi-arrow-right"></i></p>
                     </div>
-                    <DataTable :value="studentList"  paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
+                    <DataTable :value="studentList" :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
                     dataKey="id" class="shadow-2">
                         <Column field="position" header="Position">
                             <template #body="value">
@@ -218,7 +217,7 @@ export default {
                                         <div class="flex flex-row align-items-center gap-4">
                                             <img :src="value.data.image" alt="" style="width: 40px; height: 40px;">
                                             <div class="flex flex-column">
-                                                <p class="font-medium">{{ value.data.name }}</p>
+                                                <p class="text-lg font-medium">{{ value.data.name }}</p>
                                             </div>
                                         </div>
                                     </div>
@@ -229,12 +228,10 @@ export default {
                             <template #body="value">
                                 <div class="flex gap-2 align-items-center">
                                     <div class="flex gap-1 flex-column align-items-center">
-                                        <div class="flex flex-row align-items-center gap-4">
-                                            <div class="flex flex-column">
-                                                <p class="font-medium">{{ value.data.rating }} %</p>
-                                                <p class="font-medium">Overall Grade</p>
+                                            <div class="flex flex-column align-items-center">
+                                                <p class="text-lg font-medium">{{ value.data.rating }} %</p>
+                                                <p class="text-sm font-meidum text-gray font-medium">Overall Grade</p>
                                             </div>
-                                        </div>
                                     </div>
                                 </div>
                             </template>
@@ -243,12 +240,10 @@ export default {
                             <template #body="value">
                                 <div class="flex gap-2 align-items-center">
                                     <div class="flex gap-1 flex-column align-items-center">
-                                        <div class="flex flex-row align-items-center gap-4">
-                                            <div class="flex flex-column">
-                                                <p class="font-medium">{{ value.data.enrolled }}</p>
-                                                <p class="font-medium">Courses</p>
+                                            <div class="flex flex-column align-items-center">
+                                                <p class="text-lg font-medium">{{ value.data.enrolled }}</p>
+                                                <p class="text-sm font-meidum text-gray font-medium">Courses</p>
                                             </div>
-                                        </div>
                                     </div>
                                 </div>
                             </template>
@@ -263,17 +258,17 @@ export default {
                 <div class="flex flex-column">
                     <p class="font-medium font-semibold text-2xl">Calander</p>
                     <div class="flex justify-content-center">
-                      <Calendar inline showWeek class="calendar" >
-                        <!-- for badge calendar -->
-                        <template #date="slotProps">
-                          <strong v-if="slotProps.date.day > 10 && slotProps.date.day < 15"
-                            class="flex flex-column justify-content-center align-items-center gap-0">
-                            <p>{{ slotProps.date.day }}</p>
-                            <Badge value="2" class="badge text-xs flex justify-content-center align-items-center"></Badge>
-                          </strong>
-                          <template v-else>{{ slotProps.date.day }}</template>
-                        </template>
-                      </Calendar>
+                        <Calendar inline showWeek class="calendar">
+                            <!-- for badge calendar -->
+                            <template #date="slotProps">
+                              <strong v-if="slotProps.date.day > 10 && slotProps.date.day < 15"
+                                class="flex flex-column justify-content-center align-items-center gap-0">
+                                <p>{{ slotProps.date.day }}</p>
+                                <Badge value="2" class="badge text-xs flex justify-content-center align-items-center"></Badge>
+                              </strong>
+                              <template v-else>{{ slotProps.date.day }}</template>
+                            </template>
+                          </Calendar>
                     </div>
                   </div>
 
@@ -306,9 +301,6 @@ export default {
 
 <style lang="scss" scoped>
 ::v-deep(.p-datatable) {
-    .p-datatable-wrapper {
-        border-radius: 10px;
-    }
     .p-datatable-thead>tr>th {
         background: #006785;
         color: white;
@@ -327,46 +319,6 @@ export default {
         }
     }
 }
-
-::v-deep(.calendar) {
-    border-radius: 20px;
-
-    .p-datepicker-calendar table td {
-      padding: 0.25rem !important;
-    }
-  
-    table td>span {
-      border-radius: 5px;
-      width: 50px;
-      height: 50px;
-      font-size: 15px;
-    }
-  
-    table td>span:focus {
-      box-shadow: none;
-    }
-  
-    table td>span.p-highlight {
-      background: #E96853;
-      color: white;
-    }
-  
-    table td>span:not(.p-highlight) .badge {
-      background: #E96853;
-      color: white;
-      border-radius: 50%;
-    }
-  
-    table td>span.p-highlight .badge {
-      background: white;
-      color: #E96853;
-      border-radius: 16px;
-    }
-  
-    &:not(.p-disabled) table td span:not(.p-highlight):not(.p-disabled) span:hover {
-      background: #E96853 !important;
-    }
-  }
 
   /* width */
   ::-webkit-scrollbar {
@@ -388,10 +340,74 @@ export default {
     ::v-deep(.calendar) {
         width: 450px;
         table td>span {
-            width: 30px;
+            width: 50px;
             height: 50px;
             font-size: 15px;
           }
+    }
+  }
+
+.text-gray {
+    color: #9F9F9F;
+}
+
+::v-deep(.calendar) {
+    border: 1px solid #D9D5EC;
+    background: #FFF;
+    max-height: 500px;
+  
+    .p-datepicker-calendar table td {
+      padding: 0.25rem !important;
+    }
+  
+    .p-datepicker {
+      overflow: hidden;
+    }
+  
+    .p-datepicker-header {
+      padding: 0.5rem;
+    }
+  
+    table tr td {
+      width: 50px;
+      height: 50px;
+      padding: 5px;
+      margin: 5px;
+      background: rgba(250, 250, 250, 0.50);
+    }
+  
+    table td>span {
+      border-radius: 5px;
+      width: 50px;
+      height: 50px;
+      font-size: 15px;
+    }
+  
+    table td>span:focus {
+      box-shadow: none;
+      background: #E96853;
+      color: white;
+    }
+  
+    table td>span.p-highlight {
+      background: #E96853;
+      color: white;
+    }
+  
+    table td>span:not(.p-highlight) .badge {
+      background: #E96853;
+      color: white;
+      border-radius: 50%;
+    }
+  
+    table td>span.p-highlight .badge {
+      background: white;
+      color: #E96853;
+      border-radius: 16px;
+    }
+  
+    &:not(.p-disabled) table td span:not(.p-highlight):not(.p-disabled) span:hover {
+      background: #E96853 !important;
     }
   }
 </style>
